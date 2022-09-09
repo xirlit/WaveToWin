baf8364cd1a2b3fcb8c770196cc3ee4231509ee3
xirlit <karel.llukas@gmail.com>
2022-09-09 16:14:31 +0200

    waving functionaliny, counting waves, rejecting multiple calls from one address

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,25 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    mapping(address => bool) addresses; 
+
     constructor() {
         console.log("fancy");
+    }
+
+    function wave() public {
+        if(addresses[msg.sender]){
+            console.log("What you waving at? You've already been here. %s!", msg.sender);
+            return;
+        }
+        totalWaves += 1;
+        addresses[msg.sender] = true;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
